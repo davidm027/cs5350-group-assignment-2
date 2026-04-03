2d7f51265f894200f2eeca79cc9fae8ed2309b21
David Malone <davidmalone@cpp.edu>
2026-04-03 22:35:12 +0100

    Add method stubs

---
+++ assignment.cpp
@@ -0,0 +1,21 @@
+// Comparison of Matrix Multiplication
+
+#include <iostream>
+#include <omp.h>
+
+// Serial algorithm
+void MM_ser() {}
+
+// Simple parallel algorithm
+void MM_Par() {}
+
+// 1D Parallel algorithm
+void MM_1D() {}
+
+// 2D Parallel algorithm
+void MM_2D() {}
+
+int main()
+{
+    std::cout << "Hello world!\n";
+}
